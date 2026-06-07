a74e4eaf4de8622d5899194980bb853969e54268
Samverrse <mistisam4@gmail.com>
2026-06-07 13:43:13 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans=[]
+        for sub in words:
+            for word in words:
+                if sub!=word and sub in word:
+                    ans.append(sub)
+        return ans
+
+
+        
