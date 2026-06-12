320d701321e835eaa7e9418f52f8a16b73022962
Samverrse <mistisam4@gmail.com>
2026-06-12 14:31:47 +0530

    Add: valid-word-square - submission-1

---
+++ Data Structures & Algorithms/valid-word-square/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        for i in range(len(words)):
+            for j in range(len(words)):
+                for word in words:
+                    if word[i]!=word[j]:
+                        return True
+        return False
+        
