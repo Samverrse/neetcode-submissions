bfee2220c629abdb772f07282f6215b6415839c7
Samverrse <mistisam4@gmail.com>
2026-07-17 23:34:00 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mylist=[]
+        mydict={"[":"]","{":"}","(":")"}
+        for i in range(len(s)):
+            if s[i] in mydict:
+                mylist.append(s[i])
+            else:
+                if len(mylist)==0:
+                    return False
+                if mydict[mylist[-1]]==s[i]:
+                    return True
+                else:
+                    return False
+        return len(mylist)==0
+                
