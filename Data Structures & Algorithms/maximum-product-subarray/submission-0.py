18f8d489033cb4c69dcd1b0bf6b00cd34828027c
Samverrse <mistisam4@gmail.com>
2026-07-17 22:50:32 +0530

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        temp=1
+        main=0
+        for num in nums:
+            if temp>=0:
+                temp*=num
+            else:
+                temp=0
+            main=max(temp,main)
+            print(main)
+        return main
+
+        
+        
+        
