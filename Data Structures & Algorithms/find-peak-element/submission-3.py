4098bfc37933b3ca2c00cec32c636e3264750547
Samverrse <mistisam4@gmail.com>
2026-07-17 19:48:59 +0530

    Add: find-peak-element - submission-3

---
+++ Data Structures & Algorithms/find-peak-element/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        ans=[]
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                ans.append(nums[i])
+            final=max(ans)
+        return nums.index(final)
+                
+        
