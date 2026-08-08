eb57356af6b3b178970b7d96bf382a91596f8845
Samverrse <mistisam4@gmail.com>
2026-08-08 20:38:20 +0530

    Update: longest-consecutive-sequence - submission-5

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -1,23 +1,17 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans=[]
-        
-        for i in range(len(nums)):
-            prod=1
-            new_list=nums[:i]+nums[i+1:]
-            for n in new_list:
-                prod*=n
-            ans.append(prod)
-        return ans
-            
-
-
-
-
-
-
-
-
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        temp=0
+        main=0
+        numss=sorted(set(nums))
+        for i in range(len(numss)-1):
+            if numss[i+1]-numss[i]==1:
+                temp+=1
+            else:
+                temp=0
+            main=max(temp,main)
+        return main+1
 
 
 
